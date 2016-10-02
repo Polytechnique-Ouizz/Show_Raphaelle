3219202c881b3c7452d0893087f52c7a100a8681
Raphaelle Mourey <raphaelle.mourey@hec.edu>
2016-10-02 18:21:39 +0200

    nouveaucommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :shows
-  get 'home/index'
 
   get 'home/index'
 
   root 'home#index'
+
+  post 'shows/:id/book' => 'shows#book'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
